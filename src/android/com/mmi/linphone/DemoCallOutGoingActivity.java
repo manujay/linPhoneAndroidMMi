0df18f6d922ef5fcefd61b7846e16bf356095dc2
Shashank Kapsime <shashank.k@mapmyindia.com>
2017-11-28 10:24:08 +0530

    Added DemoCalling Activity #Manujay

---
+++ src/android/com/mmi/linphone/DemoCallOutGoingActivity.java
@@ -0,0 +1,12 @@
+package com.mmi.linphone;
+
+import org.linphone.LinphoneGenericActivity;
+
+/**
+ * Created by ceinfo on 11/28/17.
+ */
+
+public class DemoCallOutGoingActivity extends LinphoneGenericActivity {
+
+
+}
